d15089509a7a5685826c5e8aa76d67ef77dbc16a
Mariia_Ostapets <mariyaostapets@gmail.com>
2024-07-04 18:05:02 +0300

    Perform homework 7

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,33 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+  const boxesContainer = document.querySelector("#boxes");
+  boxesContainer.innerHTML = '';
+  const elements = [];
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    elements.push(box);
+  }
+  boxesContainer.append(...elements);
+}
+
+function destroyBoxes() {
+  document.querySelector("#boxes").innerHTML = '';
+}
+
+document.querySelector("[data-create]").addEventListener("click", () => {
+  const input = document.querySelector("#controls input");
+  const amount = parseInt(input.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = "";
+  }
+});
+document.querySelector("[data-destroy]").addEventListener("click", () => {
+  destroyBoxes();
+});
