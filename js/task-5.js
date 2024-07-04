816b1bda080c3539e8c760c47e25ea3893d254e7
Mariia_Ostapets <mariyaostapets@gmail.com>
2024-07-04 16:17:03 +0300

    Perform task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,10 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const changeColor = document.querySelector(".change-color");
+changeColor.addEventListener("click", () => {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  document.querySelector(".color").textContent = newColor;
+});
+
