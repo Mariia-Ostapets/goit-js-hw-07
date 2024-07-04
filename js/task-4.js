dfe0a4684ca2edb858a6372733710ccd43058b15
Mariia_Ostapets <mariyaostapets@gmail.com>
2024-07-04 15:51:28 +0300

    Perform task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,16 @@
-
+const registerForm = document.querySelector(".login-form");
+registerForm.addEventListener("submit", event => {
+    event.preventDefault();
+    const loginForm = event.target;
+    const email = loginForm.elements.email.value.trim();
+    const password = loginForm.elements.password.value.trim();
+    if (email === "" || password === "") {
+        return alert("All form fields must be filled in");
+    }
+    const formFieldsObject = {
+        Email: email,
+        Password: password,
+    };
+    console.log(formFieldsObject);
+    loginForm.reset();
+});
